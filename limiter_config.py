a540b1462efc4e01c2cf34062855fd7ba4e7b6ca
rishitaido <rishitaido@gmail.com>
2025-06-26 17:58:36 -0400

    limiter update

--- limiter_config.py
+++ limiter_config.py
@@ -8,11 +8,11 @@ from flask_limiter import Limiter
 from flask_limiter.util import get_remote_address
 
 # Redis URL — adjust if needed (Docker, prod, etc)
-redis_url = "redis://localhost:6379"
+memory_url = "memory://"
 
 # Create Limiter instance with Redis backend
 limiter = Limiter(
     key_func=get_remote_address,
     default_limits=["20 per minute"],
-    storage_uri=redis_url
+    storage_uri=memory_url
 )
